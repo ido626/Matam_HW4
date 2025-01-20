dade234e07349c312edeec86df81a24c4c2c86fe
Your Name <you@example.com>
2025-01-20 17:17:42 +0200

    Last updates in MatamStory and Event

--- Events/Event.cpp
+++ Events/Event.cpp
@@ -11,24 +11,29 @@ string Event::getDescription() const{return""; }
 std::vector<std::string> Event::events {}; // initialization of events vector
 std::set<std::string> allGameEvents = {"Snail", "Balrog" ,"Slime", "Pack", "SolarEclipse", "PotionMerchant" };// initialization of all known game events set
 
-/** Special Events**/
-void SolarEclipse(Player& player) {
-        if (player.getJob() == "Magician") {
-            player.CombatPower() += 1;
-        }else {player.CombatPower -= 1;}
-
-}
-
-void PotionMerchant(Player& player) {
-    if (player.getBehavior == "Responsible") {
-            while (player.getMaxHp() > player.getHealth() && player.getCoins() >= 5 ) {
-                player.setHealth() += 10;
-                player.setCoins() -= 5;
-            }
-    }else if (player.getBehavior == "RiskTaking") {
-            if (player.getHealthPoints() < 50 && player.getCoins()>=5) {
-                player.setHealth() += 10;
-                player.setCoins() -= 5;
-            }
+std::unique_ptr<Event> createEvent(const std::string & eventName) {
+    if (eventName == "Snail") {
+        return std::make_unique<Snail>();
     }
+    else if (eventName == "Balrog") {
+        return std::make_unique<Balrog>();
+    }
+    else if (eventName == "Slime") {
+        return std::make_unique<Slime>();
+    }
+    else if (eventName == "Pack") {
+        return std::make_unique<Pack>();
+    }
+    else if (eventName == "SolarEclipse") {
+        return std::make_unique<SolarEclipse>();
+    }
+    else if (eventName == "PotionMerchant") {
+        return std::make_unique<PotionMerchant>();
+    }
+    else {throw std::runtime_error("Invalid Events File");}
 }
+
+void playEvent(const Player & player,std::make_unique<monster>()) {
+    if (player.getCombatPower() > )
+}
+////////////// your here //////////////////////
